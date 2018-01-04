54cc9a326246427038ccda714496dbe017e62dbc
navono <navono007@gmail.com>
2018-01-04 15:22:28 +0800

    增加vscode调试配置文件。但是renderer的断点还是无法断入

--- src/main.development.js
+++ src/main.development.js
@@ -57,7 +57,7 @@ app.on('ready', () =>
   });
 
   if (process.env.NODE_ENV === 'development') {
-    mainWindow.openDevTools();
+    // mainWindow.openDevTools();
     mainWindow.webContents.on('context-menu', (e, props) => {
       const { x, y } = props;
 
